ae69b06e8079cf61c3dc453b71456a9a08e0b265
dcharubini <dcharubini@gmail>
2013-05-29 19:30:28 -0400

    Volatility interpreter

---
+++ src/main/java/com/zygon/trade/mtgox/data/interpreter/VolatilityInterpreter.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+
+package com.zygon.trade.mtgox.data.interpreter;
+
+import com.zygon.trade.market.Message;
+import com.zygon.trade.market.model.indication.Aggregation;
+import com.zygon.trade.market.model.indication.numeric.Volatility;
+import com.zygon.trade.market.util.MovingAverage;
+import com.zygon.trade.mtgox.data.Ticker;
+import static com.zygon.trade.mtgox.data.interpreter.MtGoxTickerInterpreter.getWindow;
+
+/**
+ *
+ * @author zygon
+ */
+public class VolatilityInterpreter extends MtGoxTickerInterpreter {
+
+    private final MovingAverage ma;
+    
+    public VolatilityInterpreter(Aggregation aggregation) {
+        super();
+        
+        if (aggregation.getType() != Aggregation.Type.AVG) {
+            throw new IllegalArgumentException("Aggregations must be based on average");
+        }
+        
+        this.ma = new MovingAverage(getWindow(aggregation));
+    }
+    
+    @Override
+    public Message[] interpret(Ticker data) {
+        this.ma.add(getMidPrice(data));
+        
+        double high = this.ma.getHigh();
+        double low = this.ma.getLow();
+        double volatility = high - low;
+        
+        return new Message[] {
+            new Volatility(data.getTradableIdentifier(), data.getTimestamp(), volatility)
+        };
+    }
+}
